39b962b91feea206f018b27aecf45768cfab8799
jenchieh <jcs090218@gmail.com>
2018-10-22 17:52:57 +0800

    Name manual name.

---
+++ website/js/config.js
@@ -0,0 +1,37 @@
+/**
+ * $File: config.js $
+ * $Date: 2018-09-28 22:33:54 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+// Manual config.
+const manual_name = "JCSEgret";
+const version_manual = "0.0.1";
+
+// Copyright text to show on the website.
+const copyright_text = "Copyright © 2018 JCSEgret Group. Built: 2018-10-22.";
+
+// Default content page.
+const intro_content = 'intro';
+// Search page.
+const search_content = 'search';
+
+// Content extension to load.
+//
+// NOTE(jenchieh): have to be the same with 'server' side's
+// config file.
+//
+// SEE : confid.js in the project root directory.
+const content_extension = '.html';
+
+// Search Input config
+const si_manual_placeholder = "Search manual...";
+const si_api_placeholder = "Search scripting...";
+
+const si_input_size = 17;  // character unit.
