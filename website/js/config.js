822b0a714b95618e3ae9777743e97ea4074b28b0
JenChieh <jcs090218@gmail.com>
2021-08-31 23:28:09 +0800

    Update config

--- website/js/config.js
+++ website/js/config.js
@@ -10,7 +10,7 @@
 "use strict";
 
 /* Manual */
-const manual_name = "JCSEgret";
+const manual_name = 'JCSEgret';
 
 /* Theme */
 const header_color = '#0B3254';
@@ -19,32 +19,32 @@ const th_show      = true;       // table header
 const th_color     = '#A2F2DE';  // table header color
 
 /* Version */
-const version_title_01 = "JCSEgret Version: ";
-const version_title_02 = "Egret Version: ";
-const version_title_03 = "";
-const version_title_04 = "";
-const version_title_05 = "";
-const version_title_06 = "";
-const version_title_07 = "";
+const version_title_01 = 'JCSEgret Version: ';
+const version_title_02 = 'Egret Version: ';
+const version_title_03 = '';
+const version_title_04 = '';
+const version_title_05 = '';
+const version_title_06 = '';
+const version_title_07 = '';
 
-const version_num_01 = "0.0.1";
-const version_num_02 = "5.2.12";
-const version_num_03 = "";
-const version_num_04 = "";
-const version_num_05 = "";
-const version_num_06 = "";
-const version_num_07 = "";
+const version_num_01 = '0.0.1';
+const version_num_02 = '5.2.12';
+const version_num_03 = '';
+const version_num_04 = '';
+const version_num_05 = '';
+const version_num_06 = '';
+const version_num_07 = '';
 
 /* Copyright */
-const copyright_text = "Copyright © 2018 JCSEgret Group. Built: 2018-10-22.";
+const copyright_text = 'Copyright © 2018 JCSEgret Group. Built: 2018-10-22.';
 
 /* Others */
 const intro_content  = 'intro';   // Default content page.
 const search_content = 'search';  // Search page.
 
 /* Homepage Link */
-const homepage_url  = "https://github.com/jcs090218/JCSEgret";  // Link to your homepage.
-const homepage_text = "JCSEgret.github";                        // Text represent your homepage.
+const homepage_url  = 'https://github.com/jcs090218/JCSEgret';  // Link to your homepage.
+const homepage_text = 'JCSEgret.github';                        // Text represent your homepage.
 
 
 // Content extension to load.
@@ -55,7 +55,7 @@ const homepage_text = "JCSEgret.github";                        // Text represen
 const content_extension = '.md';
 
 /* Search Input Text */
-const si_manual_placeholder = "Search manual...";
-const si_api_placeholder    = "Search scripting...";
+const si_manual_placeholder = 'Search manual...';
+const si_api_placeholder    = 'Search scripting...';
 
 const si_input_size = 17;  // character unit.
